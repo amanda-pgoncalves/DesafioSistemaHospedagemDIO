6cd36c1b08f16d440e955c363d519ffc56e30a6d
unknown <amandapinheirog@outlook.com>
2022-10-24 14:07:56 -0400

    Adicionando o desafio no meu respositório

---
+++ trilha-net-explorando-desafio-main/Models/Reserva.cs
@@ -0,0 +1,53 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("Capacidade de hospedes para a reserva, é menor que o número de hospedes recebido.");
+            }
+
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                decimal desconto = 0.1M * valor;
+                valor = valor - desconto;
+            }
+
+            return valor;
+        }
+    }
+}
